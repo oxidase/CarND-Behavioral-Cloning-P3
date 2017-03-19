367149a99e4c757e2c5c08ad7ca184c41886aeaf
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-03-19 11:25:38 +0100

    added 2 convolution layers

--- training.py
+++ training.py
@@ -13,16 +13,24 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 print(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
 from keras.utils import plot_model
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255. - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
 model.add(Dense(1))
 plot_model(model, to_file='model.png', show_shapes=True)
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
